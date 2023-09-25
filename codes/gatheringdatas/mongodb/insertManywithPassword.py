a3e192299102fa120d5a829ecaf8fe5844c7a260
yojulab <otter.oh@gmail.com>
2023-09-25 18:22:46 +0900

    add somethings

---
+++ codes/gatheringdatas/mongodb/insertManywithPassword.py
@@ -0,0 +1,31 @@
+# edit ..\bin\mongod.cfg
+# bindIp: 0.0.0.0
+# > use admin       # in MonogoDB
+# > db.createUser({user: "yojulab",pwd: "!yojulab*",
+#   roles: [{ role: "readWrite", db: "study_test" }]})
+
+from pymongo import MongoClient
+
+# MongoDB connection information
+host = "mongodb://192.168.0.54:27017"
+username = "yojulab"
+password = "!yojulab*"
+database_name = "study_test"  # Replace with your actual database name
+collection_name = "some_lives"  # Replace with your actual collection name
+
+# Data to be inserted
+data = [
+    {"name": "Ram", "age": "26", "city": "Hyderabad"},
+    {"name": "Rahim", "age": "27", "city": "Bangalore"}
+]
+
+# Connect to MongoDB
+client = MongoClient(host, username=username, password=password)
+db = client[database_name]
+collection = db[collection_name]
+
+# Insert data into the collection
+collection.insert_many(data)
+
+# Close the connection
+client.close()
